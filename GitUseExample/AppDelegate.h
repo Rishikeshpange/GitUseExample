433f05b70652c4303ec1d9264991ccada4138f31
Abhishek <abhishek@Abhisheks-iMac.local>
2015-06-23 13:00:23 +0530

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Abhishek on 23/06/15.
+//  Copyright (c) 2015 Tata Motors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Abhishek on 23/06/15.
+//  Copyright (c) 2015 Tata Motors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
